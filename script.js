757c4001e309bc968ba2071bd4c002ce0c2f64f4
muzammilali5994 <muzammilali5994@outlook.com>
2026-03-15 14:21:29 +0500

    part 1 complete

---
+++ script.js
@@ -0,0 +1,11 @@
+gsap.to("#box",{
+    x:500,
+    duration:2,
+    delay:1,
+    rotate:360,
+    backgroundColor:"blue",
+    borderRadius:"50%",
+    stagger:1,
+    repeat : -1,
+    yoyo:true
+})
